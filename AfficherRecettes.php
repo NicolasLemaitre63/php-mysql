8999d75ad5b32a3b238ef908b2d550058c99ce25
Elise BLIN <eb504530@iut21.u-bourgogne.fr>
2023-09-06 11:50:36 +0200

    Les tableaux associatifs

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -1,10 +1,20 @@
 <?php
 
 // Déclaration du tableau des recettes
+
 $recipes = [
-['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
-['Couscous','[...]','mickael.andrieu@exemple.com',false,],
-];
+[
+    'title' => 'Cassoulet',
+    'recipe' => 'Etape 1 : des flageolets !',
+    'author' => 'mickael.andrieu@exemple.com',
+    'enabled' => true,
+],
+[
+    'title' => 'Couscous',
+    'recipe' => 'Etape 1 : prenez une belle escalope !',
+    'author' => 'mathieu.nebra@exemple.com',
+    'enabled' => false,
+]];
 
 ?>
 
@@ -12,14 +22,15 @@ $recipes = [
 <html>
     <head>
         <title>Affichage des recettes</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <ul>
-            <?php for ($lines = 0; $lines <= 1; $lines++): ?>
-            <li>
-                <?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')';?>
-            </li>
-            <?php endfor; ?>
-        </ul>
+        <h1>Affichage des recettes</h1>
+        <?php foreach($recipes as $recipe):?>
+            <h3><?php echo $recipe['title'];?></h3>
+            <p><?php echo $recipe['recipe']?></p>
+            <p class="italic"><?php echo $recipe['author']?></p>
+            <br>
+        <?php endforeach; ?>
     </body>
 </html>
