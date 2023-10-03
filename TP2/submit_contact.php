b6c872a939dbce32f070c2276a6b3a3aea016a74
NicolasLemaitre63 <99412482+NicolasLemaitre63@users.noreply.github.com>
2023-10-03 16:18:31 +0200

    Traitez l'envoi en PHP

--- TP2/submit_contact.php
+++ TP2/submit_contact.php
@@ -27,6 +27,28 @@
         <?php include_once('header.php'); ?>
         
         
+        <?php
+        // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+        if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0)
+        {
+            // Testons si le fichier n'est pas trop gros
+            if ($_FILES['screenshot']['size'] <= 1000000)
+            {
+                // Testons si l'extension est autorisée
+                $fileInfo = pathinfo($_FILES['screenshot']['name']);
+                $extension = $fileInfo['extension'];
+                $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+                if (in_array($extension, $allowedExtensions))
+                {
+                    // On peut valider le fichier et le stocker définitivement
+                    move_uploaded_file($_FILES['screenshot']['tmp_name'],
+                    'uploads/' . basename($_FILES['screenshot']['name']));
+                    echo "L'envoi a bien été effectué !";
+                }
+            }
+        }
+        ?>
+
     <?php if((isset($_POST['email']) || filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) || (isset($_POST['message']) || empty($_POST['message']))) 
     {
     ?>
