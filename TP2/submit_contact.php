5f28a3cbeb704d7db39bd8aadfae763850ae9396
NicolasLemaitre63 <99412482+NicolasLemaitre63@users.noreply.github.com>
2023-10-03 16:02:49 +0200

    Privilégiez la méthode POST

--- TP2/submit_contact.php
+++ TP2/submit_contact.php
@@ -27,15 +27,15 @@
         <?php include_once('header.php'); ?>
         
         
-    <?php if((isset($_GET['email']) || filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) || (isset($_GET['message']) || empty($_GET['message']))) 
+    <?php if((isset($_POST['email']) || filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) || (isset($_POST['message']) || empty($_POST['message']))) 
     {
     ?>
         <h1>Message bien reçu !</h1>
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Rappel de vos informations</h5>
-                <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?> </p>
-                <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?> </p>
+                <p class="card-text"><b>Email</b> : <?php echo $_POST['email']; ?> </p>
+                <p class="card-text"><b>Message</b> : <?php echo $_POST['message']; ?> </p>
             </div>
         </div>
     <?php 
