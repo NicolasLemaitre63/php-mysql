2d75874386c4f4e9832e47be65e79c52d7b292e8
NicolasLemaitre63 <99412482+NicolasLemaitre63@users.noreply.github.com>
2023-10-18 14:21:33 +0200

    Créer un routeur

---
+++ TP4/src/controllers/homepage.php
@@ -0,0 +1,11 @@
+<?php
+// src/controllers/homepage.php
+
+require_once('src/model.php');
+
+function homepage()
+{
+    $posts = getPosts();
+
+    require('templates/homepage.php');
+}
--- TP4/templates/layout.php
+++ TP4/templates/layout.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title><? = $title ?> </title>
+        <title><?= $title ?> </title>
         <link href="style.css" rel="stylesheet" />
     </head>
     <body>
-        <? = $content ?>
+        <?= $content ?>
     </body>
 </html>
