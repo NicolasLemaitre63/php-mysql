080c7be8956456bbaedda2ff9970720b374eafd9
NicolasLemaitre63 <99412482+NicolasLemaitre63@users.noreply.github.com>
2023-12-20 11:45:14 +0100

    S as Single Responsability Principe

--- TP9/exo-solid-1/Uploader.php
+++ TP9/exo-solid-1/Uploader.php
@@ -1,11 +1,17 @@
 <?php
 
+require_once('Resizer.php');
+require_once('FileInformation.php');
+
 class Uploader
 {
     private $name;
     private $type;
+    public $temporaryName;
+    public $error;
     public $directory = '';
     public $validTypes = [];
+
 
     public function __construct($file)
     {
@@ -44,55 +50,13 @@ class Uploader
 
     public function getExtension()
     {
-        return pathinfo($this->name, PATHINFO_EXTENSION);
+        $fileInformation = new FileInformation();
+        return $fileInformation->getExtension($this->name);
     }
 
     public function resize($origin, $destination, $width, $maxHeight)
     {
-        $type = $this->getExtension();
-        $pngFamily = ['PNG', 'png'];
-        $jpegFamily = ['jpeg', 'jpg', 'JPG'];
-        if (in_array($type, $jpegFamily)) {
-            $type = 'jpeg';
-        } elseif (in_array($type, $pngFamily)) {
-            $type = 'png';
-        }
-        $function = 'imagecreatefrom' . $type;
-
-        if (!is_callable($function)) {
-            return false;
-        }
-
-        $image = $function($origin);
-
-        $imageWidth = \imagesx($image);
-        if ($imageWidth < $width) {
-            if (!copy($origin, $destination)) {
-                throw new Exception("Impossible de copier le fichier {$origin} vers {$destination}");
-            }
-        } else {
-            $imageHeight = \imagesy($image);
-            $height = (int) (($width * $imageHeight) / $imageWidth);
-            if ($height > $maxHeight) {
-                $height = $maxHeight;
-                $width = (int) (($height * $imageWidth) / $imageHeight);
-            }
-            $newImage = \imagecreatetruecolor($width, $height);
-
-            if ($newImage !== false) {
-                \imagecopyresampled($newImage, $image, 0, 0, 0, 0, $width, $height, $imageWidth, $imageHeight);
-
-                $function = 'image' . $type;
-
-                if (!is_callable($function)) {
-                    return false;
-                }
-
-                $function($newImage, $destination);
-
-                \imagedestroy($newImage);
-                \imagedestroy($image);
-            }
-        }
+        $resizer = new Resizer();
+        $resizer->resize($this, $origin, $destination, $width, $maxHeight);
     }
 }
